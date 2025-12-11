d1470c5e7a2e14554dd6464f389d12499aa38b08
SinaEinMahabadi2005 <sinamahabadi84@gmail.com>
2025-12-11 13:30:24 +0330

    Add Framer Motion

--- client/src/Pages/Home/Category/CategoryCard/index.jsx
+++ client/src/Pages/Home/Category/CategoryCard/index.jsx
@@ -2,24 +2,52 @@ import { Box, Typography } from "@mui/material";
 import React from "react";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 import { useNavigate } from "react-router-dom";
+import { motion } from "framer-motion"; 
+
 export default function CategoryCard({ category }) {
   const navigate = useNavigate();
   return (
     <Box
       sx={{
-        
         gap: "32px",
-        backgroundColor: "#F3F5F7",
+        // backgroundColor: "#F3F5F7",
         height: "100%",
         width: "100%",
-        position:'relative'
+        position: "relative",
+        perspective: "1000px", 
+        cursor:'pointer' ,
       }}
+      onClick={() =>
+          navigate(
+            `/products/${category.id}/${category.title.replaceAll(" ", "-")}`
+          )
+        }
     >
-      <Typography component={"h3"} variant="h4" sx={{ fontWeight: "bold", position:'absolute',left:'48px',top:'48px' }}>
+      <Typography
+        component={"h3"}
+        variant="h4"
+        sx={{
+          fontWeight: "bold",
+          position: "absolute",
+          left: "48px",
+          top: "48px",
+          zIndex:'1000'
+        }}
+      >
         {category?.title}
       </Typography>
+
       <Typography
-        sx={{ cursor: "pointer", display: "flex", alignItems: "center", position:'absolute',left:'48px',top:'100px' , borderBottom:'1px solid black' }}
+        sx={{
+          cursor: "pointer",
+          display: "flex",
+          alignItems: "center",
+          position: "absolute",
+          left: "48px",
+          top: "100px",
+          zIndex:'1000' ,
+          borderBottom: "1px solid black",
+        }}
         onClick={() =>
           navigate(
             `/products/${category.id}/${category.title.replaceAll(" ", "-")}`
@@ -28,10 +56,25 @@ export default function CategoryCard({ category }) {
       >
         Shop Now <ArrowForwardIcon />
       </Typography>
-      <img
+
+      
+      <motion.img
         src={import.meta.env.VITE_FILE_URL + category?.image?.url}
         alt={category?.title}
-        style={{ height: "100%", width: "100%" }}
+        style={{
+          height: "100%",
+          width: "100%",
+          borderRadius: "12px",
+          boxShadow: "0 10px 20px rgba(0,0,0,0.2)",
+          transformStyle: "preserve-3d",
+        }}
+        whileHover={{
+          scale: 1.05,
+          rotateX: 10,
+          rotateY: 10,
+          boxShadow: "0 20px 40px rgba(0,0,0,0.3)",
+        }}
+        transition={{ duration: 0.4, ease: "easeOut" }}
       />
     </Box>
   );
