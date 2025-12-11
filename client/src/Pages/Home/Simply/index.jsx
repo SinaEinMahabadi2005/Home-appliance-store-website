de85d55a36850ab256f60b26d601136a1894b2b3
SinaEinMahabadi2005 <sinamahabadi84@gmail.com>
2025-12-11 11:29:10 +0330

    Add Simplay

---
+++ client/src/Pages/Home/Simply/index.jsx
@@ -0,0 +1,18 @@
+import { Box, Typography } from '@mui/material'
+import React from 'react'
+
+export default function Simply() {
+  return (
+    <Box sx={{
+        display :'flex' ,
+        justifyContent : 'space-between',
+        alignItems : 'center' ,
+
+    }}>
+      <Typography component={'h2'} variant='h1'>
+        Simply Unique <br/> Simply Better
+      </Typography>
+      <Typography sx={{fontWeight:'bold', opacity:'0.5'}}>3legant is a gift & decorations store based in HCMC,<br/> Vietnam. Est since 2019. </Typography>
+    </Box>
+  )
+}
--- client/src/Pages/Home/index.jsx
+++ client/src/Pages/Home/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import MainSlider from './MainSlider'
+import Simply from './Simply'
 
 export default function Home() {
   return (
     <>
       <MainSlider/>
+      <Simply/>
     </>
   )
 }
